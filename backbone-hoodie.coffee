8e1b711d106c41a95f75cc570b81001c5e3b91be
Niklas Närhinen <niklas@narhinen.net>
2013-05-23 22:40:56 +0300

    Make it CommonJS compatible

--- backbone-hoodie.coffee
+++ backbone-hoodie.coffee
@@ -1,44 +1,54 @@
-Backbone.connect = (url) ->
-  Backbone.hoodie = new Hoodie url
+factory = (Backbone, Hoodie) ->
+  Backbone.connect = (url) ->
+    Backbone.hoodie = new Hoodie url
 
-Backbone.sync = (method, modelOrCollection, options) ->
-  {id, attributes, type} = modelOrCollection
-  type                 or= modelOrCollection.model.prototype.type
+  Backbone.sync = (method, modelOrCollection, options) ->
+    {id, attributes, type} = modelOrCollection
+    type                 or= modelOrCollection.model.prototype.type
 
-  promise = switch method
-    when "read"
-      if id
-        Backbone.hoodie.store.find type, id
-      else
-        Backbone.hoodie.store.findAll type
+    promise = switch method
+      when "read"
+        if id
+          Backbone.hoodie.store.find type, id
+        else
+          Backbone.hoodie.store.findAll type
 
-    when "create" then Backbone.hoodie.store.add type, attributes
-    when "update" then Backbone.hoodie.store.update type, id, attributes
-    when "delete" then Backbone.hoodie.store.remove type, id
+      when "create" then Backbone.hoodie.store.add type, attributes
+      when "update" then Backbone.hoodie.store.update type, id, attributes
+      when "delete" then Backbone.hoodie.store.remove type, id
 
-  promise.done options.success if options.success
-  promise.fail options.error if options.error
+    promise.done options.success if options.success
+    promise.fail options.error if options.error
 
-# simple merge strategy: remote always wins.
-# Feel free to overwrite.
-Backbone.Model::merge           = (attributes) ->
-  @set attributes, remote: true
+  # simple merge strategy: remote always wins.
+  # Feel free to overwrite.
+  Backbone.Model::merge           = (attributes) ->
+    @set attributes, remote: true
 
-# Make Collections listen to events.
-Backbone.Collection::initialize = ->
-  if @model
-    type = @model.prototype.type
-    @fetch()
+  # Make Collections listen to events.
+  Backbone.Collection::initialize = ->
+    if @model
+      type = @model.prototype.type
+      @fetch()
 
-    if type
-      Backbone.hoodie.store.on    "add:#{type}", (attributes) =>
-        @add attributes
+      if type
+        Backbone.hoodie.store.on    "add:#{type}", (attributes) =>
+          @add attributes
 
-      Backbone.hoodie.store.on "remove:#{type}", (attributes,options) =>
-        id = attributes.id
-        @get(id)?.destroy options
-
-      Backbone.hoodie.store.on "update:#{type}", (attributes,options) =>
-        if options.remote
+        Backbone.hoodie.store.on "remove:#{type}", (attributes,options) =>
           id = attributes.id
-          @get(id)?.merge attributes
+          @get(id)?.destroy options
+
+        Backbone.hoodie.store.on "update:#{type}", (attributes,options) =>
+          if options.remote
+            id = attributes.id
+            @get(id)?.merge attributes
+
+  return Backbone
+
+if module?.exports && require
+  Backbone = require "backbone"
+  Hoodie = require "hoodie"
+  module.exports = factory(Backbone, Hoodie)
+else
+  factory(@Backbone, @Hoodie)
--- backbone-hoodie.js
+++ backbone-hoodie.js
@@ -1,71 +1,81 @@
 (function() {
-  Backbone.connect = function(url) {
-    return Backbone.hoodie = new Hoodie(url);
-  };
+  var Backbone, Hoodie, factory;
 
-  Backbone.sync = function(method, modelOrCollection, options) {
-    var attributes, id, promise, type;
+  factory = function(Backbone, Hoodie) {
+    Backbone.connect = function(url) {
+      return Backbone.hoodie = new Hoodie(url);
+    };
+    Backbone.sync = function(method, modelOrCollection, options) {
+      var attributes, id, promise, type;
 
-    id = modelOrCollection.id, attributes = modelOrCollection.attributes, type = modelOrCollection.type;
-    type || (type = modelOrCollection.model.prototype.type);
-    promise = (function() {
-      switch (method) {
-        case "read":
-          if (id) {
-            return Backbone.hoodie.store.find(type, id);
-          } else {
-            return Backbone.hoodie.store.findAll(type);
-          }
-          break;
-        case "create":
-          return Backbone.hoodie.store.add(type, attributes);
-        case "update":
-          return Backbone.hoodie.store.update(type, id, attributes);
-        case "delete":
-          return Backbone.hoodie.store.remove(type, id);
+      id = modelOrCollection.id, attributes = modelOrCollection.attributes, type = modelOrCollection.type;
+      type || (type = modelOrCollection.model.prototype.type);
+      promise = (function() {
+        switch (method) {
+          case "read":
+            if (id) {
+              return Backbone.hoodie.store.find(type, id);
+            } else {
+              return Backbone.hoodie.store.findAll(type);
+            }
+            break;
+          case "create":
+            return Backbone.hoodie.store.add(type, attributes);
+          case "update":
+            return Backbone.hoodie.store.update(type, id, attributes);
+          case "delete":
+            return Backbone.hoodie.store.remove(type, id);
+        }
+      })();
+      if (options.success) {
+        promise.done(options.success);
       }
-    })();
-    if (options.success) {
-      promise.done(options.success);
-    }
-    if (options.error) {
-      return promise.fail(options.error);
-    }
-  };
+      if (options.error) {
+        return promise.fail(options.error);
+      }
+    };
+    Backbone.Model.prototype.merge = function(attributes) {
+      return this.set(attributes, {
+        remote: true
+      });
+    };
+    Backbone.Collection.prototype.initialize = function() {
+      var type,
+        _this = this;
 
-  Backbone.Model.prototype.merge = function(attributes) {
-    return this.set(attributes, {
-      remote: true
-    });
-  };
+      if (this.model) {
+        type = this.model.prototype.type;
+        this.fetch();
+        if (type) {
+          Backbone.hoodie.store.on("add:" + type, function(attributes) {
+            return _this.add(attributes);
+          });
+          Backbone.hoodie.store.on("remove:" + type, function(attributes, options) {
+            var id, _ref;
 
-  Backbone.Collection.prototype.initialize = function() {
-    var type,
-      _this = this;
-
-    if (this.model) {
-      type = this.model.prototype.type;
-      this.fetch();
-      if (type) {
-        Backbone.hoodie.store.on("add:" + type, function(attributes) {
-          return _this.add(attributes);
-        });
-        Backbone.hoodie.store.on("remove:" + type, function(attributes, options) {
-          var id, _ref;
-
-          id = attributes.id;
-          return (_ref = _this.get(id)) != null ? _ref.destroy(options) : void 0;
-        });
-        return Backbone.hoodie.store.on("update:" + type, function(attributes, options) {
-          var id, _ref;
-
-          if (options.remote) {
             id = attributes.id;
-            return (_ref = _this.get(id)) != null ? _ref.merge(attributes) : void 0;
-          }
-        });
+            return (_ref = _this.get(id)) != null ? _ref.destroy(options) : void 0;
+          });
+          return Backbone.hoodie.store.on("update:" + type, function(attributes, options) {
+            var id, _ref;
+
+            if (options.remote) {
+              id = attributes.id;
+              return (_ref = _this.get(id)) != null ? _ref.merge(attributes) : void 0;
+            }
+          });
+        }
       }
-    }
+    };
+    return Backbone;
   };
+
+  if ((typeof module !== "undefined" && module !== null ? module.exports : void 0) && require) {
+    Backbone = require("backbone");
+    Hoodie = require("hoodie");
+    module.exports = factory(Backbone, Hoodie);
+  } else {
+    factory(this.Backbone, this.Hoodie);
+  }
 
 }).call(this);
